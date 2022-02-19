5598e3cf5abbca28ced74ed056b9a73a8e14ada1
Felipe Ros Pegini <56928106+Felipe-16@users.noreply.github.com>
2022-02-19 12:20:45 -0300

    Primeiros passos em javascript

---
+++ Aula01/index.js
@@ -0,0 +1,26 @@
+// const nome = "Jose"
+// const endereco = `Rua k, 12`
+// const sexo = `M`
+// const idade = 27
+
+// let a = 2
+// a = "abc"
+// let nomeCompleto = "Joao Silva"
+
+// console.log(nome);
+
+// var linguagem = 'Javascript';
+// console.log('Aprendendo ' + linguagem);
+
+// var linguagem = 'java';
+// console.log('Aprendendo ' + linguagem);
+
+var idade = 28;
+console.log('oi ' + nome);
+
+if (idade >= 18){
+    var nome = 'Joao'
+    console.log('Parabens '+ nome);
+    console.log('Voce pode dirigir');
+}
+console.log('até logo ' + nome);
