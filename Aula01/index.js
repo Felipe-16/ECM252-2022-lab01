ab894433dbbc52b08568044b9f6e7198cd3fc71b
Felipe Ros Pegini <56928106+Felipe-16@users.noreply.github.com>
2022-02-26 12:14:00 -0300

    update index.js

--- Aula01/index.js
+++ Aula01/index.js
@@ -16,6 +16,7 @@
 // console.log('Aprendendo ' + linguagem);
 
 var idade = 28;
+console.log(typeof(idade));
 console.log('oi ' + nome);
 
 if (idade >= 18){
@@ -24,3 +25,55 @@ if (idade >= 18){
     console.log('Voce pode dirigir');
 }
 console.log('até logo ' + nome);
+
+// Coerçao implicita:
+
+const n1 = 2;
+const n2 = '5';
+const n3 = n1 + n2;
+console.log(n3);
+
+// Coerçao explicita:
+const n4 = n1+ Number(n2);
+console.log(n4);
+
+
+const v1 = [];
+console.log(v1.length);
+v1[0] = 3.5;
+v1[10]= 'abc';
+console.log(v1);
+console.log(v1.length);
+
+// List <Intenger> list = new ArrayList <>(2);
+// list.add(2);
+// list.add(3);
+// list.add(5);
+
+const v2 = [2, 'abc', true];
+
+
+const nomes = [
+    'Ana',
+    'Antonio',
+    'Rodrigo',
+    'Alex',
+    'Cristina'
+]
+
+console.log(nomes);
+
+const apenasA = nomes.filter((n) => {
+    return n.startsWith('A');
+})
+console.log(apenasA);
+
+const mapeamento = nomes.map((nome) => {
+    return nome.charAt(0);
+})
+console.log(mapeamento);
+
+const resultado = nomes.every((n) => {
+    return n.startsWith('A');
+} )
+console.log(resultado);
